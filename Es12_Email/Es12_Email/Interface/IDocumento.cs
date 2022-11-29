a910c20cb421aa025ed199197299d35450849654
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2022-11-29 10:47:03 +0100

    Edit, validators created, visual form is not complete

---
+++ Es12_Email/Es12_Email/Interface/IDocumento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es12_Email.Interface
+{
+    public interface IDocumento : IVisualizza
+    {
+        string Titolo { get; set; }
+
+        string Path { get; set; }
+    }
+}
---
+++ Es12_Email/Es12_Email/Validators/EmailValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Es12_Email.Validators
+{
+    public static class EmailValidator
+    {
+        private static Regex _validationRegex = new Regex(
+            @"^\p{L}+@\w+.\w+$",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public static bool Validate(string input) => _validationRegex.IsMatch(input);
+
+    }
+}
