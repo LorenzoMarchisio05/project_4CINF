5466203852f2a7fe61a4b4ffa6b12b0154f56081
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2022-11-29 10:07:51 +0100

    edit: fine es Regex

--- Es13_Regex/Es13_Regex/Validators/SpecValidator.cs
+++ Es13_Regex/Es13_Regex/Validators/SpecValidator.cs
@@ -5,7 +5,9 @@ namespace Es13_Regex
 {
     public sealed class SpecValidator : IValidator
     {
-        private static Regex _validationRegex = new Regex(@"^(INF|MEC|ELT|TUR|AFM)$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static Regex _validationRegex = new Regex(
+            @"^(INF|MEC|ELT|TUR|AFM)$", 
+            RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         public bool Validate(string input) => _validationRegex.IsMatch(input);
     }
