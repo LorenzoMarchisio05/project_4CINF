06c53e90661c56d2bd9097ef5e7d798446ef4ede
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2022-12-16 13:19:55 +0100

    ES18_FormEreditati: init

---
+++ ES18_EreditarietaForm/ES18_EreditarietaForm/frmBase.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ES18_EreditarietaForm
+{
+    partial class frmBase
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTesto = new System.Windows.Forms.Label();
+            this.txtTesto = new System.Windows.Forms.TextBox();
+            this.btnAnnulla = new System.Windows.Forms.Button();
+            this.btnInvia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTesto
+            // 
+            this.lblTesto.AutoSize = true;
+            this.lblTesto.Location = new System.Drawing.Point(9, 9);
+            this.lblTesto.Name = "lblTesto";
+            this.lblTesto.Size = new System.Drawing.Size(35, 13);
+            this.lblTesto.TabIndex = 0;
+            this.lblTesto.Text = "label1";
+            // 
+            // txtTesto
+            // 
+            this.txtTesto.Location = new System.Drawing.Point(12, 25);
+            this.txtTesto.Name = "txtTesto";
+            this.txtTesto.Size = new System.Drawing.Size(176, 20);
+            this.txtTesto.TabIndex = 1;
+            // 
+            // btnAnnulla
+            // 
+            this.btnAnnulla.Location = new System.Drawing.Point(12, 51);
+            this.btnAnnulla.Name = "btnAnnulla";
+            this.btnAnnulla.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnulla.TabIndex = 2;
+            this.btnAnnulla.Text = "Annulla";
+            this.btnAnnulla.UseVisualStyleBackColor = true;
+            // 
+            // btnInvia
+            // 
+            this.btnInvia.Location = new System.Drawing.Point(111, 51);
+            this.btnInvia.Name = "btnInvia";
+            this.btnInvia.Size = new System.Drawing.Size(75, 23);
+            this.btnInvia.TabIndex = 3;
+            this.btnInvia.Text = "Invia";
+            this.btnInvia.UseVisualStyleBackColor = true;
+            // 
+            // frmBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 80);
+            this.Controls.Add(this.btnInvia);
+            this.Controls.Add(this.btnAnnulla);
+            this.Controls.Add(this.txtTesto);
+            this.Controls.Add(this.lblTesto);
+            this.Name = "frmBase";
+            this.Text = "Form Base";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTesto;
+        private System.Windows.Forms.TextBox txtTesto;
+        private System.Windows.Forms.Button btnAnnulla;
+        private System.Windows.Forms.Button btnInvia;
+    }
+}
+
