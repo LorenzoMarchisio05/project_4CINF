bc8922fbae7b671e0f6f2e1c0e3742e7f9b990eb
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2023-03-06 11:34:56 +0100

    update

---
+++ Es21_EsercizioDB/Es21_EsercizioDB/SQLQuery3.sql
@@ -0,0 +1,11 @@
+﻿select ALUNNI.* 
+from ALUNNI, ASSENZE
+where ASSENZE.idAlunno = ALUNNI.idAlunno;
+
+select ALUNNI.*
+from ALUNNI, ASSENZE
+where (ASSENZE.idAlunno = ALUNNI.idAlunno) and (ASSENZE.dataAssenza = CONVERT(DATE, GETDATE()));
+
+select *
+from ASSENZE
+where dataAssenza >= CONVERT(DATE, '15/01/2023', 103) and dataAssenza <= CONVERT(DATE, GETDATE());
