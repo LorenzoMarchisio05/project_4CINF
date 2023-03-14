0338046a2259d9359f76d29053afc39fbb7f0286
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2023-03-14 10:46:42 +0100

    added new queries

---
+++ Es21_EsercizioDB/Es21_EsercizioDB/SQLQuery5.sql
@@ -0,0 +1,21 @@
+﻿update ALUNNI
+set altezza = altezza + 1
+where citta = 'savigliano';
+
+-- visualizza cognome nome alunno voto più alto
+
+select top 1 with ties 
+ALUNNI.nome + ' ' + ALUNNI.cognome, ALUNNI.voto
+from ALUNNI, VOTI
+where ALUNNI.idAlunno = VOTI.idAlunno
+order by VOTi.voto desc;
+
+select ALUNNI.nome + ' ' + ALUNNI.cognome, VOTI.voto
+from ALUNNI, VOTI
+where ALUNNI.idAlunno = VOTI.idAlunno
+	and VOTI.voto = (select max(voto) from VOTI);
+
+select ALUNNI.nome + ' ' + ALUNNI.cognome, AVG(VOTI.voto) as media
+from ALUNNI, VOTI
+where ALUNNI.idAlunno = VOTI.idAlunno
+group by ALUNNI.nome, ALUNNI.cognome;
