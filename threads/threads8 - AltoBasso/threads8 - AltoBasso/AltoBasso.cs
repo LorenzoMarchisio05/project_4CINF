38433210fc6e5c1c566dc0d67a0c949e66a87a41
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2023-04-18 11:28:29 +0200

    Created AltoBasso Game using threads

---
+++ threads/threads8 - AltoBasso/threads8 - AltoBasso/AltoBasso.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace threads8___AltoBasso
+{
+    internal partial class Program
+    {
+        public static class AltoBasso
+        {
+            private static readonly Random rnd = new Random(420);
+
+            private static int numeroSegreto;
+
+            private static int tentativi = 0;
+
+            private static int start = 0, end = 100;
+
+            public static void Run()
+            {
+                generaNumeroSegreto();
+
+                Thread indovina = new Thread(Indovina);
+
+                indovina.Start("alto;101");
+            }
+
+            private static void generaNumeroSegreto()
+            {
+                numeroSegreto = rnd.Next(100) + 1;
+                tentativi = 0;
+            }
+
+            private static void Indovina(object data)
+            {
+                tentativi++;
+                var split = data.ToString().Split(';');
+                string esito = split[0];
+                int valore = int.Parse(split[1]);
+
+                if (esito.ToLower() == "alto")
+                {
+                    end = valore - 1;
+                }
+                else
+                {
+                    start = valore + 1;
+                }
+
+                var numeroGenerato = rnd.Next(start, end + 1);
+
+                Console.WriteLine($"Numero Generato: {numeroGenerato}");
+
+                Thread controlla = new Thread(Controlla);
+
+                controlla.Start(numeroGenerato);
+            }
+
+            private static void Controlla(object data)
+            {
+                var numero = (int)data;
+
+                if (numero == numeroSegreto)
+                {
+                    Console.WriteLine($"indovinato: {numero} -> {numeroSegreto}");
+                    return;
+                }
+
+                Thread indovina = new Thread(Indovina);
+
+                if (numero > numeroSegreto)
+                {
+                    indovina.Start($"alto;{numero}");
+                    return;
+                }
+
+                if (numero < numeroSegreto)
+                {
+                    indovina.Start($"basso;{numero}");
+                }
+            }
+        }
+    }
+
+}
---
+++ threads/threads8 - AltoBasso/threads8 - AltoBasso/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threads8___AltoBasso
+{
+    internal partial class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            AltoBasso.Run();
+
+            Console.ReadKey();
+        }
+    }
+
+}
