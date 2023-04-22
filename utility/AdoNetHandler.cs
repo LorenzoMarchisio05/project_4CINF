9f0c3e9f82dad19744b0bd2bc02e11757bf9e067
unknown <l.marchisio.2268@vallauri.net>
2023-04-22 12:30:06 +0200

    update

---
+++ utility/AdoNetHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AdoNetSqlHandler
+{
+    class AdoNetSqlHandler
+    {
+        private SqlConnection connection;
+       
+        public AdoNetSqlHandler(string dbName)
+        {
+            this.connection = initConnection(dbName);
+        }
+
+
+        private SqlConnection initConnection(string dbName)
+        {
+        
+            var connectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={Application.StartupPath}\\{dbName};Integrated Security=True;Connect Timeout=30";
+            var connection = new SqlConnection
+            {
+                ConnectionString = cnString,
+            };
+            
+            connection.Open();
+
+            return connection;
+        }
+        
+
+        public DataTable ExecuteQuery(SqlCommand command)
+        {
+            command.Connection = this.connection;
+
+            var dataTable = new DataTable();
+
+            var adapter = new SqlDataAdapter(command);
+            
+            adapter.Fill(dataTable);
+           
+            return dataTable;
+        }
+        
+
+        public DataTable ExecuteQuery(SqlCommand command, DataGridView dgv)
+        {
+            command.Connection = this.connection;
+
+            var dataTable = new DataTable();
+
+            var adapter = new SqlDataAdapter(command);
+            
+            adp.Fill(dataTable);
+            dgv.DataSource = dataTable;
+           
+            return dataTable;
+        }
+       
+
+        public int ExecuteNonQuery(SqlCommand command)
+        {
+            command.Connection = this.connection;
+           
+            return command.ExecuteNonQuery();
+        }
+        
+        public object ExecuteScalar(SqlCommand command)
+        {
+            command.Connection = this.connection;
+            
+            return cmd.ExecuteScalar();
+        }
+    }
+}
