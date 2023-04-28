ae2c4bec6faee0871f69c5631d862f74d9122883
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2023-04-28 12:53:35 +0200

    update exercise concurrency, used locks, mutex, semaphore, semaphore slim

--- threads/thread10 - Programmazione concorrente/thread10 - Programmazione concorrente/FrmThread.Designer.cs
+++ threads/thread10 - Programmazione concorrente/thread10 - Programmazione concorrente/FrmThread.Designer.cs
@@ -57,7 +57,7 @@
             this.dgvLock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvLock.Location = new System.Drawing.Point(12, 51);
             this.dgvLock.Name = "dgvLock";
-            this.dgvLock.Size = new System.Drawing.Size(201, 387);
+            this.dgvLock.Size = new System.Drawing.Size(201, 700);
             this.dgvLock.TabIndex = 1;
             // 
             // dgvMutex
@@ -65,7 +65,7 @@
             this.dgvMutex.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvMutex.Location = new System.Drawing.Point(220, 51);
             this.dgvMutex.Name = "dgvMutex";
-            this.dgvMutex.Size = new System.Drawing.Size(201, 387);
+            this.dgvMutex.Size = new System.Drawing.Size(201, 700);
             this.dgvMutex.TabIndex = 3;
             // 
             // btnMutex
@@ -74,7 +74,7 @@
             this.btnMutex.Name = "btnMutex";
             this.btnMutex.Size = new System.Drawing.Size(201, 23);
             this.btnMutex.TabIndex = 2;
-            this.btnMutex.Text = "Butex";
+            this.btnMutex.Text = "Mutex";
             this.btnMutex.UseVisualStyleBackColor = true;
             this.btnMutex.Click += new System.EventHandler(this.btnMutex_Click);
             // 
@@ -83,7 +83,7 @@
             this.dgvSemaphore.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvSemaphore.Location = new System.Drawing.Point(427, 51);
             this.dgvSemaphore.Name = "dgvSemaphore";
-            this.dgvSemaphore.Size = new System.Drawing.Size(201, 387);
+            this.dgvSemaphore.Size = new System.Drawing.Size(201, 700);
             this.dgvSemaphore.TabIndex = 5;
             // 
             // btnSemaphore
@@ -101,7 +101,7 @@
             this.dgvSemaphoreSlim.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvSemaphoreSlim.Location = new System.Drawing.Point(634, 51);
             this.dgvSemaphoreSlim.Name = "dgvSemaphoreSlim";
-            this.dgvSemaphoreSlim.Size = new System.Drawing.Size(201, 387);
+            this.dgvSemaphoreSlim.Size = new System.Drawing.Size(201, 700);
             this.dgvSemaphoreSlim.TabIndex = 7;
             // 
             // btnSemaphoreSlim
@@ -118,7 +118,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(847, 450);
+            this.ClientSize = new System.Drawing.Size(852, 763);
             this.Controls.Add(this.dgvSemaphoreSlim);
             this.Controls.Add(this.btnSemaphoreSlim);
             this.Controls.Add(this.dgvSemaphore);
@@ -127,8 +127,10 @@
             this.Controls.Add(this.btnMutex);
             this.Controls.Add(this.dgvLock);
             this.Controls.Add(this.btnLock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FrmThread";
             this.Text = "Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmThread_FormClosing);
             this.Load += new System.EventHandler(this.FrmThread_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvLock)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMutex)).EndInit();
--- threads/thread10 - Programmazione concorrente/thread10 - Programmazione concorrente/FrmThread.cs
+++ threads/thread10 - Programmazione concorrente/thread10 - Programmazione concorrente/FrmThread.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,34 +14,286 @@ namespace thread10___Programmazione_concorrente
 {
     public partial class FrmThread : Form
     {
+        private static object _lock = new object();
+
+        private static Mutex _mutex = new Mutex(false);
+
+        private static Semaphore _semaphore = new Semaphore(2, 2);
+
+        private static SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(2, 2);
+
         public FrmThread()
         {
             InitializeComponent();
+
         }
 
         private void FrmThread_Load(object sender, EventArgs e)
         {
+        }
+
+        private void initDataGridView(DataGridView dgv)
+        {
+            dgv.RowHeadersVisible = dgv.ColumnHeadersVisible = false;
+            dgv.ColumnCount = 1;
+
+            dgv.Columns[0].Width = dgvLock.Width;
             
+            dgv.Rows.Clear();
+
+            dgv.ReadOnly = true;
         }
 
         private void btnLock_Click(object sender, EventArgs e)
         {
+            btnLock.Enabled = false;
+            initDataGridView(dgvLock);
 
+            var thread1 = new Thread(lock_handler)
+            { 
+                Name = "thread 1"
+            };
+
+            var thread2 = new Thread(lock_handler)
+            {
+                Name = "thread 2"
+            };
+
+            var thread3 = new Thread(lock_handler)
+            {
+                Name = "thread 3"
+            };
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
         }
 
         private void btnMutex_Click(object sender, EventArgs e)
         {
+            btnMutex.Enabled = false;
+            initDataGridView(dgvMutex);
 
+            var thread1 = new Thread(mutex_handler)
+            {
+                Name = "thread 1"
+            };
+
+            var thread2 = new Thread(mutex_handler)
+            {
+                Name = "thread 2"
+            };
+
+            var thread3 = new Thread(mutex_handler)
+            {
+                Name = "thread 3"
+            };
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
         }
 
         private void btnSemaphore_Click(object sender, EventArgs e)
         {
+            btnSemaphore.Enabled = false;
+            initDataGridView(dgvSemaphore);
 
+            var thread1 = new Thread(semaphore_handler)
+            {
+                Name = "thread 1"
+            };
+
+            var thread2 = new Thread(semaphore_handler)
+            {
+                Name = "thread 2"
+            };
+
+            var thread3 = new Thread(semaphore_handler)
+            {
+                Name = "thread 3"
+            };
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
         }
 
         private void btnSemaphoreSlim_Click(object sender, EventArgs e)
         {
+            btnSemaphoreSlim.Enabled = false;
+            initDataGridView(dgvSemaphoreSlim);
 
+            var thread1 = new Thread(semaphoreSlim_handler)
+            {
+                Name = "thread 1"
+            };
+
+            var thread2 = new Thread(semaphoreSlim_handler)
+            {
+                Name = "thread 2"
+            };
+
+            var thread3 = new Thread(semaphoreSlim_handler)
+            {
+                Name = "thread 3"
+            };
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        #region Handlers
+        private void lock_handler()
+        {
+            var s = "";
+
+            var operation = new Action(() => {
+
+                dgvLock.Rows.Add(s);
+                btnLock.Enabled = true;
+            });
+
+
+            lock (_lock)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(1000);
+
+                    s = $"{Thread.CurrentThread.Name}-> Sezione critica: {i}";
+
+                    this.BeginInvoke(operation);
+                }
+            }
+
+            // Istruzioni non bloccanti
+            /*
+            for (int i = 10; i >= 0; i--)
+            {
+                Thread.Sleep(1000);
+
+                s = $"{Thread.CurrentThread.Name}-> Sezione non bloccante: {i}";
+
+                this.Invoke(operation);
+            }
+            */
+        }
+
+        private void mutex_handler()
+        {
+            var s = "";
+
+            var operation = new Action(() => {
+
+                dgvMutex.Rows.Add(s);
+                btnMutex.Enabled = true;
+            });
+
+            _mutex.WaitOne();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+
+                s = $"{Thread.CurrentThread.Name}-> Sezione critica: {i}";
+
+                this.BeginInvoke(operation);
+            }
+
+            _mutex.ReleaseMutex();
+
+            // Istruzioni non bloccanti
+            /*
+            for (int i = 10; i >= 0; i--)
+            {
+                Thread.Sleep(1000);
+
+                s = $"{Thread.CurrentThread.Name}-> Sezione non bloccante: {i}";
+
+                this.Invoke(operation);
+            }
+            */
+            
+        }
+
+        private void semaphore_handler()
+        {
+            var s = "";
+
+            var operation = new Action(() => {
+
+                dgvSemaphore.Rows.Add(s);
+                btnSemaphore.Enabled = true;
+            });
+
+            _semaphore.WaitOne();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+
+                s = $"{Thread.CurrentThread.Name}-> Sezione critica: {i}";
+
+                this.BeginInvoke(operation);
+            }
+
+            _semaphore.Release();
+
+            // Istruzioni non bloccanti
+            /*
+            for (int i = 10; i >= 0; i--)
+            {
+                Thread.Sleep(1000);
+
+                s = $"{Thread.CurrentThread.Name}-> Sezione non bloccante: {i}";
+
+                this.Invoke(operation);
+            }
+            */
+        }
+
+        private void semaphoreSlim_handler()
+        {
+            var s = "";
+
+            var operation = new Action(() => {
+
+                dgvSemaphoreSlim.Rows.Add(s);
+                btnSemaphoreSlim.Enabled = true;
+            });
+
+            _semaphoreSlim.Wait();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+
+                s = $"{Thread.CurrentThread.Name}-> Sezione critica: {i}";
+
+                this.BeginInvoke(operation);
+            }
+
+            _semaphoreSlim.Release();
+
+            // Istruzioni non bloccanti
+            /*
+            for (int i = 10; i >= 0; i--)
+            {
+                Thread.Sleep(1000);
+
+                s = $"{Thread.CurrentThread.Name}-> Sezione non bloccante: {i}";
+
+                this.Invoke(operation);
+            }
+            */
+        }
+
+        #endregion
+
+        private void FrmThread_FormClosing(object sender, FormClosingEventArgs e)
+        {
         }
     }
 }
