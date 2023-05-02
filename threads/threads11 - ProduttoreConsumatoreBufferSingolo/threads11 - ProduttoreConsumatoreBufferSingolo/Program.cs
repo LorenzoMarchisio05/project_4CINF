e92c3e16cd9fb4454c69f842d79dab59b4c35bf4
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2023-05-02 11:32:49 +0200

    es 11 threads using producer consumer pattern

---
+++ threads/threads11 - ProduttoreConsumatoreBufferSingolo/threads11 - ProduttoreConsumatoreBufferSingolo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threads11___ProduttoreConsumatoreBufferSingolo
+{
+    internal class Program
+    {
+        private const int NUMERO_DATI = 10;
+        private const int TEMPO_PRODUZIONE = 200;
+        private const int TEMPO_CONSUMO = 500;
+
+
+        private static SemaphoreSlim _emptySemaphore = new SemaphoreSlim(1);
+
+        private static SemaphoreSlim _fullSemaphore = new SemaphoreSlim(0);
+
+        private static int buffer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"full semaphore -> current count: {_fullSemaphore.CurrentCount}");
+
+            Console.WriteLine("sull semaphore -> release");
+            _fullSemaphore.Release();
+
+            Console.WriteLine($"full semaphore -> current count: {_fullSemaphore.CurrentCount}");
+            
+            Console.WriteLine("sull semaphore -> wait");
+            _fullSemaphore.Wait();
+
+            Console.Read();
+        }
+    }
+}
+
