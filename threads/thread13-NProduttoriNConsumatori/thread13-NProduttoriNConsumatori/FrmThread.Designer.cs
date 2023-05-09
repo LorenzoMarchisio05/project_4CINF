7878c5e1f049e5f575b8598bf1f55360d9b8fb4a
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2023-05-09 11:37:27 +0200

    update es thread

---
+++ threads/thread13-NProduttoriNConsumatori/thread13-NProduttoriNConsumatori/FrmThread.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace thread13_NProduttoriNConsumatori
+{
+    partial class FrmThread
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pbBuffer = new System.Windows.Forms.ProgressBar();
+            this.lblBuffer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(28, 22);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(137, 22);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pbBuffer
+            // 
+            this.pbBuffer.Location = new System.Drawing.Point(28, 93);
+            this.pbBuffer.Name = "pbBuffer";
+            this.pbBuffer.Size = new System.Drawing.Size(584, 23);
+            this.pbBuffer.TabIndex = 2;
+            // 
+            // lblBuffer
+            // 
+            this.lblBuffer.AutoSize = true;
+            this.lblBuffer.Location = new System.Drawing.Point(25, 131);
+            this.lblBuffer.Name = "lblBuffer";
+            this.lblBuffer.Size = new System.Drawing.Size(35, 13);
+            this.lblBuffer.TabIndex = 3;
+            this.lblBuffer.Text = "label1";
+            // 
+            // FrmThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 150);
+            this.Controls.Add(this.lblBuffer);
+            this.Controls.Add(this.pbBuffer);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmThread";
+            this.Text = "N Produttori, M Consumatori";
+            this.Load += new System.EventHandler(this.FrmThread_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar pbBuffer;
+        private System.Windows.Forms.Label lblBuffer;
+    }
+}
+
---
+++ threads/thread13-NProduttoriNConsumatori/thread13-NProduttoriNConsumatori/FrmThread.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thread13_NProduttoriNConsumatori
+{
+    public partial class FrmThread : Form
+    {
+
+        private const int producerNumber = 5;
+
+        private const int consumerNumber = 2;
+
+        private const int bufferSize = 4;
+
+        private const int productionTime = 5 * 1000;
+
+        private const int consumeTime = 1 * 1000;
+
+        private static SemaphoreSlim emptySemaphore = new SemaphoreSlim(bufferSize);
+
+        private static SemaphoreSlim fullSemaphore = new SemaphoreSlim(0);
+
+        private static object lockProducerConsumer = new object();
+
+        private static int[] buffer = new int[bufferSize];
+
+        private int put = 0;
+
+        private int remove = 0;
+
+        private bool stopProduction = false;
+
+        private int bufferElementNumber = 0;
+
+        private int producedElement;
+
+        private static Random rnd = new Random(420);
+
+        public FrmThread()
+        {
+            InitializeComponent();
+
+            init();
+        }
+
+        private void init()
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            pbBuffer.Minimum = 0;
+            pbBuffer.Maximum = bufferSize;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var productionThreads = Enumerable.Range(0, producerNumber)
+                .Select(i => new Thread(produce) { Name = $"Thread Produci{i}"});
+
+            var consumerThreads = Enumerable.Range(0, producerNumber)
+                .Select(i => new Thread(consume) { Name = $"Thread Consuma{i}" });
+
+            Parallel.ForEach(productionThreads, (thread) => thread.Start());
+            Parallel.ForEach(consumerThreads, (thread) => thread.Start());
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            put = remove = 0;
+            stopProduction = false;
+
+            producedElement = 0;
+        }
+
+        private void consume()
+        {
+            string name = Thread.CurrentThread.Name;
+
+            var updateProgressBar = new Action(() =>
+            {
+                pbBuffer.Value = bufferElementNumber;
+            });
+
+            var updateLabel = new Action(() =>
+            {
+                lblBuffer.Text = readBuffer();
+            });
+
+            while (!stopProduction || emptyBuffer())
+            {
+                fullSemaphore.Wait();
+
+                lock (lockProducerConsumer)
+                {
+
+                    buffer[remove] = 0;
+
+                    bufferElementNumber--;
+
+                    remove = (remove + 1) % bufferSize;
+
+                    this.Invoke(updateProgressBar);
+                    this.Invoke(updateLabel);
+                }
+
+                Thread.Sleep(rnd.Next(1, consumeTime));
+
+                emptySemaphore.Release();
+            }
+        }
+
+        private bool emptyBuffer() => buffer
+            .All(element => element == 0);
+
+        private void produce()
+        {
+            string name = Thread.CurrentThread.Name;
+
+            var updateProgressBar = new Action(() =>
+            {
+                pbBuffer.Value = bufferElementNumber;
+            });
+
+            var updateLabel = new Action(() =>
+            {
+                lblBuffer.Text = readBuffer();
+            });
+
+            while (!stopProduction)
+            {
+                emptySemaphore.Wait();
+
+                lock (lockProducerConsumer)
+                {
+                    producedElement++;
+
+                    buffer[put] = producedElement;
+
+                    bufferElementNumber++;
+
+                    put = (put + 1) % bufferSize;
+
+                    this.Invoke(updateProgressBar);
+                    this.Invoke(updateLabel);
+                }
+
+                Thread.Sleep(rnd.Next(1, productionTime));
+
+                fullSemaphore.Release();
+            }
+        }
+
+        private string readBuffer()
+        {
+            string s = "";
+
+            foreach(var data in buffer)
+            {
+                s += $", {data}";
+            }
+
+            return $"{s.Substring(1)} -> {bufferElementNumber}<-";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+
+            stopProduction = true;
+        }
+
+        private void FrmThread_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
